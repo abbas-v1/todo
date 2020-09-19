4e22b2a56b0017a31da00affd2b6e4247817fad7
Muhammad Abbas <abbas@bitoasis.net>
2020-09-19 14:06:35 +0400

    jpa entity for project

---
+++ src/main/java/com/abbas/todo/entity/Project.java
@@ -0,0 +1,22 @@
+package com.abbas.todo.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.Data;
+
+/**
+ * Entity for Project
+ *
+ * @author abbas
+ */
+@Data
+@Entity
+public class Project implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String projectName;
+
+}
