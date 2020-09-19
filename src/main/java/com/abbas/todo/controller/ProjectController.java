959e3a78907cf95fd1127d1e7ac2c401d9547b0d
Muhammad Abbas <abbas@bitoasis.net>
2020-09-19 16:29:54 +0400

    project controller resource

---
+++ src/main/java/com/abbas/todo/controller/ProjectController.java
@@ -0,0 +1,31 @@
+package com.abbas.todo.controller;
+
+import com.abbas.todo.entity.Project;
+import com.abbas.todo.service.ProjectService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for Project
+ *
+ * @author abbas
+ */
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping
+    public List<Project> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+
+    @GetMapping("/{projectName}")
+    public Project getProject(@PathVariable("projectName") String projectName) {
+        return projectService.getProject(projectName);
+    }
+
+}
