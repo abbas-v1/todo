b18715ec8759c9d97fd9512bac4923f72d983ab7
Muhammad Abbas <abbas@bitoasis.net>
2020-09-19 16:29:14 +0400

    project service to get projects from database

---
+++ src/main/java/com/abbas/todo/service/ProjectService.java
@@ -0,0 +1,32 @@
+package com.abbas.todo.service;
+
+import com.abbas.todo.entity.Project;
+import com.abbas.todo.repository.ProjectRepository;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for Project
+ *
+ * @author abbas
+ */
+@Service
+@Slf4j
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public List<Project> getAllProjects() {
+        log.debug("Get all projects");
+        return projectRepository.findAll();
+    }
+
+    public Project getProject(String projectName) {
+        log.debug("Get project by name : {}", projectName);
+        return projectRepository.findByProjectName(projectName);
+    }
+
+}
