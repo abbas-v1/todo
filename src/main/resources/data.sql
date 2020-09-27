9c85210a1b6cf3afc2251bf0fb7715a1753cbb59
Muhammad Abbas <abbas@bitoasis.net>
2020-09-27 09:56:18 +0400

    remove public from insert sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
 /*========================================================================================*/
-INSERT INTO public.project(id, project_name) VALUES
+INSERT INTO project(id, project_name) VALUES
 (1, 'Maintenance'),
 (2, 'Interior Designing');
 
 /*========================================================================================*/
-INSERT INTO public.user_info(id, user_name) VALUES
+INSERT INTO user_info(id, user_name) VALUES
 (1, 'John'),
 (2, 'Sarah'),
 (3, 'Tom');
 
 /*========================================================================================*/
-INSERT INTO public.todo(id, task, project_id, user_id, completed) VALUES
+INSERT INTO todo(id, task, project_id, user_id, completed) VALUES
 (1, 'Repainting the walls', 1, 1, false),
 (2, 'Polishing the doors', 1, 3, false),
 (3, 'Fixing the tiles', 1, 1, false),
